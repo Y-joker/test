c7383da8dd99b993417f7b3fdb4525ba72e29557
yjoker <1054180994@qq.com>
2020-03-04 14:53:01 +0800

    测试

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String args[]){
         System.out.println("test success");
-        System.out.println("测试");
+        System.out.println("测 试");
     }
 }
